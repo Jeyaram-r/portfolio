b98b497ef219dbeafd842ea726557365dd6de90d
jeyaramr8652@gmail.com <jeyaramr8652@icanio.school>
2026-04-08 16:18:41 +0530

    feat: update routing to navigate to /portfolio instead of /home

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -13,8 +13,8 @@ import Home from "../modules/home";
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigate to="/portfolio" />} />
+      <Route path="/portfolio" element={<Home />} />
     </Routes>
   );
 }
